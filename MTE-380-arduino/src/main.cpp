b1fd9716cadd639f52cf6f725fae290820db3168
Luke Zhang <LukeZhang0826@gmail.com>
2026-01-30 12:17:51 -0500

    interfacing pi and arduino

--- MTE-380-arduino/src/main.cpp
+++ MTE-380-arduino/src/main.cpp
@@ -1,12 +1,30 @@
 #include <Arduino.h>
 
+unsigned long blinkDelay = 100; // ms
+unsigned long lastToggle = 0;
+bool ledState = false;
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(100);
+  // --- handle serial input ---
+  if (Serial.available()) {
+    long val = Serial.parseInt();   // read number from serial
+    if (val > 0 && val <= 5000) {   // sanity bounds
+      blinkDelay = val;
+      Serial.print("Blink delay set to ");
+      Serial.println(blinkDelay);
+    }
+  }
+
+  // --- non-blocking blink ---
+  unsigned long now = millis();
+  if (now - lastToggle >= blinkDelay) {
+    lastToggle = now;
+    ledState = !ledState;
+    digitalWrite(LED_BUILTIN, ledState);
+  }
 }
--- hello.py
+++ hello.py
@@ -1,3 +1,24 @@
-print(
-    "hello"
-)
+import serial
+import time
+
+ser = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
+time.sleep(2)  # wait for Arduino reset
+
+print("Type a number (ms) to change blink rate. Ctrl+C to exit.")
+
+try:
+    while True:
+        val = input("> ").strip()
+        if not val:
+            continue
+
+        # basic validation
+        if not val.isdigit():
+            print("Enter a number in milliseconds")
+            continue
+
+        ser.write((val + "\n").encode())
+except KeyboardInterrupt:
+    print("\nExiting...")
+finally:
+    ser.close()
